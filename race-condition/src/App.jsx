3ead071c2405eca62053670d9ce5393544c88b93
Praveen1104 <impraveenbosco@gmail.com>
2025-12-02 17:57:04 +0530

    race-condition

---
+++ race-condition/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+const App = () => {
+  const [categories, setCaatregories] = useState([]);
+  const fetchProducts = async () => {
+    const response = fetch(" https://api.escuelajs.co/api/v1/categories");
+    response
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        setCaatregories(data);
+      });
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <div className="outer-div">
+        {categories.map((data, i) => {
+          return (
+            <div key={i} className="single-card">
+              <p>{data.name}</p>
+              <button>Show</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
+//https://fakeapi.platzi.com/en/rest/categories/
