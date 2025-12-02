30acd0072801f4236c0bcc3b68cfc08099f3aa1a
Praveen1104 <impraveenbosco@gmail.com>
2025-12-02 14:25:06 +0530

    chips add removal

---
+++ chips/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChipsInput from "./components/ChipsInput";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <ChipsInput />
+    </div>
+  );
+};
+
+export default App;
