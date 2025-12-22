c131e21789f027e24fb06786bbefd32514433dd1
praveen1104 <impraveenbosco@gmail.com>
2025-12-23 00:12:13 +0530

    progress

---
+++ progressbar/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Progressbar from './components/Progressbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Progressbar />
+    </>
+  )
+}
+
+export default App
---
+++ progressbar/src/components/Progressbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Progressbar = () => {
+  return (
+    <div className='progress'>
+      <span>45%</span>
+    </div>
+  )
+}
+
+export default Progressbar
